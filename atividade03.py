46ad4728ae9fc3a8fa97dc7022f7e8a638a04b15
baconzitus <thiagolds50@gmail.com>
2020-09-24 17:56:36 -0300

    feat: atividade03

---
+++ atividade03.py
@@ -0,0 +1,11 @@
+def inverter(nome_2):
+    return nome_2[::-1]
+
+nome= input("insara uma palavra: ")
+print("nomal:",nome)
+
+print("ivertido:", inverter(nome))
+
+#ou
+#nome = inverter(nome)
+#print(nome)
